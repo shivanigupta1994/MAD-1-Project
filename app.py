d99843e8727745cd31ff18f44aa6e53e1cdf0808
shivanigupta1994 <gshivani392@gmail.com>
2023-07-20 14:26:13 +0530

    creating sign-in sign-up page

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ app=Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/sign-in")
+def sign():
+    return render_template("sign-in.html")
+
+@app.route("/sign-up")
+def sig():
+    return render_template("sign-up.html")
+
 
 if __name__== "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
+
+
